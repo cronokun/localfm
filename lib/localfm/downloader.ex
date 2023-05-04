caf4408cfc73c7f96ca3c43575e79d1d19035b38
cronokun <crono.neko@gmail.com>
2023-05-04 15:25:40 +0300

    Fix playhistory file path

--- lib/localfm/downloader.ex
+++ lib/localfm/downloader.ex
@@ -1,7 +1,11 @@
 defmodule LocalFM.Downloader do
+  @moduledoc """
+  TODO: Move URL and creds to dotfiles
+  """
+
   @curl_bin "/opt/homebrew/opt/curl/bin/curl"
   @creds "pi:moodeaudio"
-  @source_url "sftp://moode.local/var/local/www/playhistory.log"
+  @source_url "sftp://moode.local/var/log/moode_playhistory.log"
 
   def retrieve_data do
     System.cmd(@curl_bin, ["--insecure", "--silent", "--user", @creds, @source_url])
